7687d808c3c54e395902358e9c3093874ff62768
Samiya <samii-1989@live.com>
2024-01-24 20:37:02 +0100

    "display pghotographers photos & city & price & other details"

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,52 @@
-    async function getPhotographers() {
+    // asynic function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        // let photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        // ]
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // return ({
+        //     photographers: [...photographers, ...photographers, ...photographers]})
+    // }
+    async function getPhotographers() {
+        try {
+            const response = await fetch('../../data/photographers.json'); // Remplacez 'URL_DU_FICHIER_JSON' par le chemin correct de votre fichier JSON 
+            
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP: ${response.status}`);
+            }
+    
+            const data = await response.json();
+            // console.log(response);
+            // console.log(data);
+            // console.log(data.photographers);
+            // console.log(data.media);
+            return data;
+            
+        } catch (error) {
+            console.error('Erreur lors de la récupération des photographes:', error.message);
+            // V gérer l'erreur ici, par exemple, afficher un message à l'utilisateur ou effectuer une action spécifique.
+        }
     }
-
+    
+    // getPhotographers();
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
@@ -39,8 +60,10 @@
     async function init() {
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
+        console.log(photographers);
         displayData(photographers);
     }
     
     init();
+    
     
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,6 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, price, tagline
+    } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +10,18 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h4 = document.createElement( 'h4' );
+        h4.textContent = city;
+        const h5 = document.createElement( 'h5' );
+        h5.textContent = tagline;
+        const p = document.createElement( 'p' );
+        p.textContent = price +'€/jour';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h4);
+        article.appendChild(h5);
+        article.appendChild(p);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, tagline, price,getUserCardDOM }
 }
