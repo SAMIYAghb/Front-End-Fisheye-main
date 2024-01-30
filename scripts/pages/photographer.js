8c8de3182108d7c8c4e5cffb3a5ac8125b18f91f
samiya <samii-1989@live.com>
2024-01-30 15:26:34 +0100

    "sets up the carousel"

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,192 +1,94 @@
-// // //Mettre le code JavaScript lié à la page photographer.html
-// import  {photographerDetailsFactory} from '../factories/photographDetailsFactory.js';
 
-
-// let params = (new URL(document.location)).searchParams;
-// let photographer_id = params.get('id');
-// let medias;
-// let photographer;
-
-// // 
-// async function getPhotographer(photographer_id) {
-//     try {
-//       const response = await fetch("../../data/photographers.json");
-//       if (!response.ok) {
-//         throw new Error(`Erreur HTTP: ${response.status}`);
-//       }
-      
-//       const data = await response.json();
-//       const photographers = data.photographers;
-      
-//       let result = null;
-      
-//       photographers.forEach(element => {
-//         if (element.id == photographer_id) {
-//           result = element;
-//         }
-//       });
-      
-//       if (result) {
-//         // console.log(result);
-//       } else {
-//         console.log("Photographe non trouvé");
-//       }
-      
-//       return result;
-//     } catch (error) {
-//       console.error("Erreur lors de la récupération des photographes:", error.message);
-//     }
-//   }
-//   // getPhotographer('243');
-
-//   async function getMediaByPhotographerId(photographer_id) {
-//     // console.log("Photographer ID:", photographer_id);
-//     try {
-//       const response = await fetch("../../data/photographers.json");
-//       if (!response.ok) {
-//         throw new Error(`Erreur HTTP: ${response.status}`);
-//       }
-  
-//       const data = await response.json();
-//       // console.log(data.media)
-//       const media = data.media;
-  
-//       // console.log(media);
-//       const mediaOfPhotographer = media.filter(item => item.photographerId == photographer_id);
-//       // console.log(mediaOfPhotographer)
-
-//       if (mediaOfPhotographer.length > 0) {
-//         // console.log(mediaOfPhotographer);
-//       } else {
-//         console.log("Aucun média trouvé pour ce photographe");
-//       }
-      
-  
-//       return mediaOfPhotographer;
-      
-
-//     } catch (error) {
-//       console.error("Erreur lors de la récupération des médias:", error.message);
-//       throw error;
-//     }
-
-//   //   const mediaOfPhotographer = {};
-    
-//   //   for (const item of media) {
-//   //     if (item.photographerId == photographer_id) {
-//   //       mediaOfPhotographer.push(item);
-//   //       // console.log(mediaOfPhotographer)
-//   //     }
-//   //   }
-
-//   //   if (mediaOfPhotographer.length > 0) {
-//   //     console.log(mediaOfPhotographer);
-//   //   } else {
-//   //     console.log("Aucun média trouvé pour ce photographe");
-//   //   }
-
-//   //   return mediaOfPhotographer;
-//   // } catch (error) {
-//   //   console.error("Erreur lors de la récupération des médias:", error.message);
-//   // }
-//   }
-//   // getMediaByPhotographerId()
-//   // getMediaByPhotographerId("82")
-
-
-
-// // Photographer's detail
-// async function displayPhotographer(photographer) {
-//   // console.log(photographer)
-//   const photographersHeader = document.querySelector(".photograph-header");
-//   // console.log(photographersHeader);
-//   const contactButton = document.querySelector(".contact_button");
-//   // console.log(contactButton);
-//   const photographerDetails = photographerDetailsFactory(photographer);
-//   // console.log(photographerDetails);
-//   const userCardDOM = photographerDetails.getUserCardDOM();
-//   // console.log(userCardDOM);
-// /*** */
-// // Utilisez la fonction pour créer un objet photographerAvatarDom
-//   const photographerAvatarDom = photographerDetailsFactory(photographer);
-// // console.log(photographerAvatarDom)
-//   // Obtenez l'avatar du photographe
-//   const photographerAvatar = photographerAvatarDom.getUserAvatarDOM();
-//   // console.log(photographerAvatar)
-// /*** */
-
-//   photographersHeader.insertBefore(userCardDOM, contactButton);
-//   photographersHeader.appendChild(photographerAvatar);
-// }
-
-// // Photographer's medias
-// async function displayPhotographerMedia(photographer) {
-
-// }
-
-// async function init() {
-//   // Récupère les medias et photographes
-//   medias = await getMediaByPhotographerId(photographer_id);
-// //   console.log(medias)
-//   photographer = await getPhotographer(photographer_id);
-//     // console.log(photographer);
-//   displayPhotographer(photographer);
-//   displayPhotographerMedia(photographer);
-  
-// }
-
-// init();
-
-import { getPhotographers } from '../utils/api.js';
-import { photographerFactory, mediaFactory  } from '../factories/photographer.js';
+import { getPhotographers } from "../utils/api.js";
+import {
+  photographerFactory,
+  mediaFactory,
+} from "../factories/photographer.js";
+import {openLightbox} from '../utils/lightbox.js' ; 
 
 // créer le header du photographe
-function headerPhotographer(photographer){
-    const photographeName = document.querySelector('.photograph-name');
-    const photographeLocation = document.querySelector('.photograph-location');
-    const photographeTagline = document.querySelector('.photograph-tagline');
-    const photographeHeader = document.querySelector(".photograph-header");
-    
-    photographeName.innerText = photographer.name;
-    photographeLocation.innerText = photographer.location;
-    photographeTagline.innerText = photographer.tagline;
-    
-    const img = document.createElement( 'img' );
-    img.setAttribute("src", photographer.portrait);
-    img.setAttribute('alt', photographer.name)
-    img.setAttribute('tabindex', 0);
-    img.classList.add('img-rounded'); 
-    photographeHeader.appendChild(img);
-  }
-  // Fonction pour afficher la galerie d'images
-function displayMedia(medias) {
-    const imagesContainer = document.getElementById('photographer-images');
-    // console.log(imagesContainer);
-    imagesContainer.innerHTML = "";
-    // console.log(medias);
-    medias.forEach((media) => {
-      const mediaModel = mediaFactory(media);
-      const mediaCardDOM = mediaModel.getMediaCardDOM();
-      imagesContainer.appendChild(mediaCardDOM);
-    });
-  }
+function headerPhotographer(photographer) {
+  const photographeName = document.querySelector(".photograph-name");
+  const photographeLocation = document.querySelector(".photograph-location");
+  const photographeTagline = document.querySelector(".photograph-tagline");
+  const photographeHeader = document.querySelector(".photograph-header");
 
-  // Fonction d'initialisation
+  photographeName.innerText = photographer.name;
+  photographeLocation.innerText = photographer.location;
+  photographeTagline.innerText = photographer.tagline;
+
+  const img = document.createElement("img");
+  img.setAttribute("src", photographer.portrait);
+  img.setAttribute("alt", photographer.name);
+  img.setAttribute("tabindex", 0);
+  img.classList.add("img-rounded");
+  photographeHeader.appendChild(img);
+}
+
+// Fonction pour afficher la galerie d'images
+function displayMedia(medias) {
+  const imagesContainer = document.getElementById("photographer-images");
+  // console.log(imagesContainer);
+  imagesContainer.innerHTML = "";
+  // console.log(medias);
+  medias.forEach((media) => {
+    const mediaModel = mediaFactory(media);
+    const mediaCardDOM = mediaModel.getMediaCardDOM();
+    imagesContainer.appendChild(mediaCardDOM);
+  });
+}
+
+
+
+// Fonction d'initialisation
 async function init() {
-    // Récupération de l'ID du photographe
-  const params = (new URL(document.location)).searchParams;
-  const photographerId = params.get('id');
+  // Récupération de l'ID du photographe
+  const params = new URL(document.location).searchParams;
+  const photographerId = params.get("id");
 
   // Récupération de l'objet photographe correspondant avec ses medias
   const photographers = await getPhotographers();
-  const photographer = photographers.find(item => item.id == photographerId);
+  // console.log(photographers);
+  const photographer = photographers.find((item) => item.id == photographerId);
+  // const photographerMedias = photographer.medias;
+  // console.log(photographer.medias);
   // console.log("photographer avec ses medias:",photographer);
   // console.log(" ses medias:",photographer.medias);
   // affichage le header du photographe
   headerPhotographer(photographer);
-//   const sortedMedias = sortMediaBy('likes',photographer.medias);
-    // affichage la galerie du photographe
-    displayMedia(photographer.medias);
+  //   const sortedMedias = sortMediaBy('likes',photographer.medias);
+  // affichage la galerie du photographe
+  displayMedia(photographer.medias);
+
+
+
+
+
+
+  // Evenement 'click' sur toute la partie main
+const main = document.getElementById('main');
+main.addEventListener('click', event => {
+
+  //  récupèration des coordonnées du click
+  const x = event.clientX; 
+  const y = event.clientY;
+  // console.log(x,y);
+  // console.log(event.target);
+  // console.log(event.target.tagName);
+  if((event.target.tagName === 'IMG' || event.target.tagName === 'VIDEO') && event.target.parentElement.className !== 'photograph-header' ){
+    const photographerMedias = photographer.medias;
+    // console.log(photographerMedias);
+      const mediaId = event.target.id;
+      // console.log(mediaId);
+      const media = photographerMedias.find(media => media.id == mediaId);
+      // console.log(media);
+      const mediaIndex = photographerMedias.indexOf(media);
+      // console.log(mediaIndex);
+      openLightbox (photographerMedias, x, y, mediaIndex);
+  }
+
+
+})
 
 }
 init();
---
+++ scripts/utils/lightbox.js
@@ -0,0 +1,106 @@
+// Ouverture de la lightbox
+function openLightbox(medias, x, y, currentIndex){
+    // console.log('hhh');
+    // l'affiche de l'a bonne 'imagequi correspond  à l'index dans le tableau des medias
+
+    function showMedia(){
+        const url = medias[currentIndex].url;
+        // console.log(url);
+        const title = medias[currentIndex].title;
+        // console.log(title);
+        const lightboxImg = document.querySelector('.lightbox-img');
+        // console.log(lightboxImg);
+        const lightboxImgContainer = lightboxImg.parentElement;
+        // console.log(lightboxImgContainer);
+   
+    // soit  'img' ou 'video'
+    if(medias[currentIndex].type == 'image'){
+        lightboxImgContainer.innerHTML = `
+          <img src="${url}" class="lightbox-img" alt="${title}" title="${title}">
+          <figcaption class="lightbox-caption">${title}</figcaption>
+        `;
+      }
+      else{
+        lightboxImgContainer.innerHTML = `
+          <video controls src="${url}" class="lightbox-img" type="video/mp4" alt="${title}" title="${title}" id="video"></video>
+          <figcaption class="lightbox-caption">${title}</figcaption>
+        `;
+      }    
+      const newLightboxImg = document.querySelector('.lightbox-img');
+    newLightboxImg.style.opacity = 0;
+    setTimeout(() => {
+        newLightboxImg.style.opacity = 1;
+      }, 100);
+      newLightboxImg.focus();
+}
+
+// initialisation des constantes des element html
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const closeButton = document.querySelector('.close');
+const lightbox = document.querySelector('.lightbox');
+const borderLightbox = document.querySelector('.border-lightbox');
+
+showMedia();
+
+//animation apre ouvrture de la lightbox utilisant le click de la souris
+lightbox.style.top = `${y}px`;
+lightbox.style.left = `${x}px`;
+lightbox.style.transform = `translate(-${x}px, -${y}px) scale(1)`;
+lightbox.style.transition = 'transform 0.5s ease-in';
+
+ // Temporisation pour afficher l'image de fond semi-transparent, le temps que la lightbox soit de taille 1
+ setTimeout(() => {
+    lightbox.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  }, 500);
+  borderLightbox.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  borderLightbox.style.transition = "background-color 0.5s cubic-bezier( 1, 0, 1, 0 )"
+
+// Ajout d'un évènement 'click' sur toute la lightbox
+lightbox.addEventListener('click', event => {
+    // Si click sur le bouton next, in incrémente l'index puis on affiche le media
+    if (event.target.className === 'next') {
+      currentIndex++;
+      if (currentIndex >= medias.length) {
+        currentIndex = 0;
+      }
+      showMedia();
+      }
+    
+    // Si click sur le bouton prev, in décrémente l'index puis on affiche le media
+    if (event.target.className === 'prev') {
+      currentIndex--;
+      if (currentIndex < 0) {
+        currentIndex = medias.length - 1;
+      }
+      showMedia();
+    }
+
+    // Si click sur la croix, on ferme la lightbox avec l'animation inverse de l'ouverture
+    if (event.target.className === 'close') {
+        document.body.style.overflow = 'auto';
+        lightbox.style.transform = `translate(-50%, -50%) scale(0)`;
+        lightbox.style.transition = 'transform 0.5s ease-in';
+        lightbox.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+        borderLightbox.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+        borderLightbox.style.transition = "background-color 0.2s ease-out"
+      }
+    });
+
+     // Prise en charge des flèches de direction du clavier et de la touche echap
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+      prevButton.click();
+    }
+
+    if (event.key === 'ArrowRight') {
+      nextButton.click();
+    }
+
+    if (event.key === 'Escape') {
+      closeButton.click();
+    }
+  });
+
+}
+export {openLightbox};
