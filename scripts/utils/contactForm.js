429c38074faa5ea94fcc9cbb35bef2b3241762b9
Samiya <samii-1989@live.com>
2024-02-28 12:22:16 +0100

    "mettre en place l'accessibilité pour le form de contacte"

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,112 +1,156 @@
 /* eslint no-unused-vars:"off" */
-function displayModal(event) {
-  const modal = document.getElementById('contact_modal');
-  // Vérifie si la touche appuyée est la touche Entrée
-  // if (event.key === 'Enter') {
-  //   event.preventDefault();
-  //   return;
-  // }
-  modal.style.display = 'flex';
-  const photographerName = document.querySelector('.photograph-name');
-  const photographerContact = document.querySelector('.modal p');
-  photographerContact.innerText = `${photographerName.innerText}`;
-  // Focus sur l'image de fermeture
-  const closeButton = document.querySelector('.modal img');
-  console.log(closeButton);
-
-  closeButton.focus();
-
-  //Le focus devrait être positionné sur le premier champ de saisie lorsque la modale est affichée.
-  // document.getElementById('prenom').focus();
-}
+// Focus sur l'image de fermeture
+const closeButton = document.querySelector('.modal img');
+console.log(closeButton);
 
 function closeModal() {
   const modal = document.getElementById('contact_modal');
   modal.style.display = 'none';
-}
-const form = document.querySelector('form');
-function displayErrors(errors) {
-  const errorMessagesContainer = document.getElementById('error-messages');
-  errorMessagesContainer.innerHTML = ''; // Clear previous error messages
-
-  // Display each error message in the appropriate element
-  Object.keys(errors).forEach((field) => {
-    const errorMessage = errors[field];
-    const errorElement = document.createElement('p');
-    errorElement.textContent = errorMessage;
-    errorMessagesContainer.appendChild(errorElement);
-  });
+  // Mettre le focus sur l'élément image après la fermeture de la modale
+  // closeButton.focus();
 }
 
-function validateForm() {
-  const errors = {};
-
-  // Validate each form field
-  const fieldsToValidate = ['prenom', 'nom', 'email', 'message'];
-
-  fieldsToValidate.forEach((field) => {
-    const fieldValue = document.getElementById(field).value;
-    // Add your validation logic here
-    // Example: Check if the field is empty
-    if (fieldValue.trim() === '') {
-      errors[field] = 'Ce champ est obligatoire.';
-    }
-    // Add more validation checks as needed
-  });
-
-  return errors;
+function displayModal(event) {
+  const modal = document.getElementById('contact_modal');
+  modal.style.display = 'flex';
+  const photographerName = document.querySelector('.photograph-name');
+  const photographerContact = document.querySelector('.modal p');
+  photographerContact.innerText = `${photographerName.innerText}`;
+  closeButton.focus();
+  // // Mettre le focus sur le premier champ du formulaire dans la modale
+  // const firstInput = document.getElementById('prenom');
+  // if (firstInput) {
+  //   firstInput.focus();
+  // }
 }
 
-function resetForm() {
-  // Clear the form fields
-  document.getElementById('prenom').value = '';
-  document.getElementById('nom').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('message').value = '';
+// closeButton.focus();
+// Ajouter un gestionnaire d'événements pour capturer la touche Tab
+// closeButton.addEventListener('keydown', function (e) {
+//   if (e.key === 'Tab') {
+//     // Empêcher le comportement par défaut (changement de focus)
+//     e.preventDefault();
+//     // Mettre le focus sur l'élément image
+//     closeButton.focus();
+//   } else if (e.key === 'Enter') {
+//     e.preventDefault();
+//     closeModal();
+//   }
+// });
 
-  // Clear error messages
-  const errorMessagesContainer = document.getElementById('error-messages');
-  errorMessagesContainer.innerHTML = '';
-}
 
-function sendForm(event) {
-  // Empêche la soumission du formulaire
-  event.preventDefault();
 
-  // Vérifier les erreurs de validation
-  const errors = validateForm();
 
-  // Récupérer les valeurs des champs
-  if (Object.keys(errors).length === 0) {
-    // Aucune erreur, continuer le traitement du formulaire
-    const formData = {
-      prenom: document.getElementById('prenom').value,
-      nom: document.getElementById('nom').value,
-      email: document.getElementById('email').value,
-      message: document.getElementById('message').value,
-    };
-    // Afficher les valeurs dans la console
-    console.log('Form Data:', formData);
+const focusableElements = 'img, input, textarea, button, [tabindex]:not([tabindex="-1"])';
+const modal = document.querySelector('#contact_modal'); // select the modal by it's id
 
-    closeModal();
-    resetForm();
-  } else {
-    // Il y a des erreurs, afficher les messages d'erreur
-    displayErrors(errors);
+const firstFocusableElement = modal.querySelector('.modal-form-img');
+console.log(firstFocusableElement);
+const focusableContent = modal.querySelectorAll(focusableElements);
+
+const lastFocusableElement = focusableContent[focusableContent.length - 1];
+// get last element to be focused inside modal
+document.addEventListener('keydown', function(e) {
+  let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+  if (!isTabPressed) {
+    return;
   }
-}
 
-// Add event listener to the form
-form.addEventListener('submit', sendForm);
+  if (e.shiftKey) { // if shift key pressed for shift + tab combination
+    if (document.activeElement === firstFocusableElement) {
+      lastFocusableElement.focus(); // add focus for the last focusable element
+      e.preventDefault();
+    }
+  } else { // if tab key is pressed
+    if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+      firstFocusableElement.focus(); // add focus for the first focusable element
+      e.preventDefault();
+    }
+  }
+});
 
-// Ajoutez un gestionnaire d'événements clavier pour le formulaire
-document.getElementById('contact_form').addEventListener('keydown', function handleFormKeyEvents(event) {
-  // Gérer les touches Enter pour soumettre le formulaire
-  if (event.key === 'Enter' && document.activeElement.tagName === 'INPUT') {
-    document.activeElement.click();
+firstFocusableElement.focus();
+
+// Adjouter un event listener pour "keydown" image
+firstFocusableElement.addEventListener('keydown', function(e) {
+  if (e.key === 'Enter') {
+    closeModal();
   }
 });
 
 
+// const form = document.querySelector('form');
+// function displayErrors(errors) {
+//   const errorMessagesContainer = document.getElementById('error-messages');
+//   errorMessagesContainer.innerHTML = ''; // Clear previous error messages
 
+//   // Display each error message in the appropriate element
+//   Object.keys(errors).forEach((field) => {
+//     const errorMessage = errors[field];
+//     const errorElement = document.createElement('p');
+//     errorElement.textContent = errorMessage;
+//     errorMessagesContainer.appendChild(errorElement);
+//   });
+// }
 
+// function validateForm() {
+//   const errors = {};
+
+//   // Validate each form field
+//   const fieldsToValidate = ['prenom', 'nom', 'email', 'message'];
+
+//   fieldsToValidate.forEach((field) => {
+//     const fieldValue = document.getElementById(field).value;
+//     // Add your validation logic here
+//     // Example: Check if the field is empty
+//     if (fieldValue.trim() === '') {
+//       errors[field] = 'Ce champ est obligatoire.';
+//     }
+//     // Add more validation checks as needed
+//   });
+
+//   return errors;
+// }
+
+// function resetForm() {
+//   // Clear the form fields
+//   document.getElementById('prenom').value = '';
+//   document.getElementById('nom').value = '';
+//   document.getElementById('email').value = '';
+//   document.getElementById('message').value = '';
+
+//   // Clear error messages
+//   const errorMessagesContainer = document.getElementById('error-messages');
+//   errorMessagesContainer.innerHTML = '';
+// }
+
+// function sendForm(event) {
+//   // Empêche la soumission du formulaire
+//   event.preventDefault();
+
+//   // Vérifier les erreurs de validation
+//   const errors = validateForm();
+
+//   // Récupérer les valeurs des champs
+//   if (Object.keys(errors).length === 0) {
+//     // Aucune erreur, continuer le traitement du formulaire
+//     const formData = {
+//       prenom: document.getElementById('prenom').value,
+//       nom: document.getElementById('nom').value,
+//       email: document.getElementById('email').value,
+//       message: document.getElementById('message').value,
+//     };
+//     // Afficher les valeurs dans la console
+//     console.log('Form Data:', formData);
+
+//     closeModal();
+//     resetForm();
+//   } else {
+//     // Il y a des erreurs, afficher les messages d'erreur
+//     displayErrors(errors);
+//   }
+// }
+
+// // Add event listener to the form
+// form.addEventListener('submit', sendForm);
