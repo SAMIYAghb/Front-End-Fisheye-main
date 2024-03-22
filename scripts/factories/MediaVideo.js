47c224606f2da9c937e5d03058d8f6866c412b48
Samiya <samii-1989@live.com>
2024-03-22 11:23:35 +0100

    "video control & link fontaesome"

--- scripts/factories/MediaVideo.js
+++ scripts/factories/MediaVideo.js
@@ -10,7 +10,7 @@ class MediaVideo extends Media {
   renderHtml() {
     const figure = document.createElement('figure');
     figure.innerHTML = `
-      <video controls src='assets/images/${this.photographerId}/${this.src}' alt='${this.title}'id='${this.id}' tabindex='0' aria-label='${this.title}'>${this.title}</video>
+      <video src='assets/images/${this.photographerId}/${this.src}' alt='${this.title}'id='${this.id}' tabindex='0' aria-label='${this.title}'>${this.title}</video>
       <figcaption class='video-caption'>${this.title}</figcaption>
       <span tabindex='0'>${this.likes}</span>
       <i tabindex='0' class='fa-regular fa-heart' aria-label='likes' role='button' title='likes' ></i>
